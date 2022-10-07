b33e872658f4be95535d2098f0df61440e0a543a
dommie123 <dman9532@gmail.com>
2022-10-06 22:34:23 -0600

    Removed boilerplate

--- test/App.test.tsx
+++ test/App.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import App from '../src/App';
 
-test('renders learn react link', () => {
+it('renders learn react link', () => {
   render(<App />);
 });
